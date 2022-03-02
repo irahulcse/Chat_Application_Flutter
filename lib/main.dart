1cc0d66c9f9e412c439bc23a9e4c748202e1e7ab
Rahul Chandra <1rahulchandra1@gmail.com>
2022-03-02 08:13:28 +0530

    Updated main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'New Chat App',
+      debugShowCheckedModeBanner: false,
       home: new HomePage(),
     );
   }
